90e0607f3910a80133b6d7140a1d75685c6848fa
jenwit <jenwit_penjamrat@hotmail.com>
2019-09-10 22:31:02 +0700

    ssss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { RegisterComponent } from './component/register/register.component';
+import { HomeComponent } from './component/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path:'',component:HomeComponent},
+  { path:'register',component:RegisterComponent}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
